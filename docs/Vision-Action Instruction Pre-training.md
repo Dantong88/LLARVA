24b295d1eab69602fc3b2abae85d72707e14015e
niudt <niudantong.88@gmail.com>
2024-10-21 01:37:33 +0000

    add files

--- docs/Vision-Action Instruction Pre-training.md
+++ docs/Vision-Action Instruction Pre-training.md
@@ -2,5 +2,35 @@
 
 For the vision-action instruction pre-training, the implementation of LLARVA is built based on [LLaVA](https://github.com/haotian-liu/LLaVA).
 
+### Installation
+1. Clone this repository
+```bash
+git clone https://github.com/Dantong88/LLARVA
+cd LLAVA
+```
 
-### 
+2. Install Package
+```Shell
+conda create -n llarva python=3.10 -y
+conda activate llarva
+pip install --upgrade pip  # enable PEP 660 support
+pip install -e .
+```
+
+3. Install additional packages for training cases
+```
+pip install -e ".[train]"
+pip install flash-attn --no-build-isolation
+
+# if you see some import errors when you upgrade,
+# please try running the command below (without #)
+# pip install flash-attn --no-build-isolation --no-cache-dir
+```
+
+### Prepare the Data
+
+1. See [DATASET.md]() to download both image.zip file and annotations, put them under ./data folder.
+2. Download off-the-shelf projector weights:
+```angular2html
+git clone https://huggingface.co/liuhaotian/llava-v1.5-mlp2x-336px-pretrain-vicuna-7b-v1.5
+```
---
+++ scripts/v1_5/vision-action_instruction_pretraining_lora.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+deepspeed --include localhost:2,3,4,5 llava/train/train_mem.py \
+    --lora_enable True --lora_r 128 --lora_alpha 256 --mm_projector_lr 2e-5 \
+    --deepspeed ./scripts/zero3.json \
+    --model_name_or_path lmsys/vicuna-7b-v1.5 \
+    --version v1 \
+    --data_path /scratch/partial_datasets/rlbench/release_oxe/annotations/train.json::/scratch/partial_datasets/rlbench/release_oxe/annotations/val.json \
+    --image_folder '/scratch/partial_datasets/rlbench/release_oxe' \
+    --vision_tower openai/clip-vit-large-patch14-336 \
+    --pretrain_mm_mlp_adapter llava-v1.5-mlp2x-336px-pretrain-vicuna-7b-v1.5/mm_projector.bin \
+    --mm_projector_type mlp2x_gelu \
+    --mm_vision_select_layer -2 \
+    --mm_use_im_start_end False \
+    --mm_use_im_patch_token False \
+    --image_aspect_ratio pad \
+    --group_by_modality_length True \
+    --bf16 True \
+    --output_dir ./output/action-vision_instruction_pretraining \
+    --num_train_epochs 1 \
+    --per_device_train_batch_size 16 \
+    --per_device_eval_batch_size 32 \
+    --gradient_accumulation_steps 1 \
+    --evaluation_strategy "steps" \
+    --eval_steps 1000 \
+    --save_strategy "steps" \
+    --save_steps 1000 \
+    --save_total_limit 1 \
+    --learning_rate 5e-5 \
+    --weight_decay 0. \
+    --warmup_ratio 0.03 \
+    --lr_scheduler_type "cosine" \
+    --logging_steps 1 \
+    --tf32 True \
+    --model_max_length 2048 \
+    --gradient_checkpointing True \
+    --dataloader_num_workers 4 \
+    --lazy_preprocess True \
+    --report_to wandb
